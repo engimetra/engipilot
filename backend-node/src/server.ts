d16bad02c85281248845046a06b9eb23727b30d7
engimetra <votre-email-github>
2026-06-02 23:36:53 +0100

    chore(server): update backend server configuration

--- backend-node/src/server.ts
+++ backend-node/src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app"
-import { env } from "@/config/env"
-import { connectDB, disconnectDB } from "@/config/database"
+import { env } from "./config/env"
+import { connectDB, disconnectDB } from "./config/database"
 
 async function bootstrap(): Promise<void> {
   try {
